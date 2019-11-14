34d95f4f6c7efa239fb8716bba3aa77492f2a8fb
Stephanie Viveros <sviveros05@gmail.com>
2019-11-14 14:32:35 -0600

    Finished Create Route

--- resources/coffees.py
+++ resources/coffees.py
@@ -6,6 +6,7 @@ from playhouse.shortcuts import model_to_dict
 
 coffee = Blueprint('coffees', 'coffee')
 
+# Index Route
 @coffee.route('/', methods=["GET"])
 def get_all_coffees():
 	try:
@@ -15,4 +16,14 @@ def get_all_coffees():
 	except models.DoesNotExist:
 		return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
-		
+#Create Route
+@coffee.route('/', methods=['POST'])
+def create_coffees():
+	payload = request.get_json()
+	print(type(payload), 'payload')
+	coffee = models.Coffee.create(**payload)
+	print(coffee.__dict__)
+	print(dir(coffee))
+	print(model_to_dict(coffee), '<<<<<<<<This is model to dict')
+	coffee_dict = model_to_dict(coffee)
+	return jsonify(data=coffee_dict, status={'code': 201, 'message': 'Created - Success'})
